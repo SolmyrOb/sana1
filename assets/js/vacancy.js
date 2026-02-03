0e89f2faff9e98fd7c71c7161655d69de80f1ce1
SolmyrOb <SolmyrK@yandex.ru>
2026-02-03 17:37:00 +0300

    Add files via upload

--- assets/js/vacancy.js
+++ assets/js/vacancy.js
@@ -1,31 +1,36 @@
-document.addEventListener('DOMContentLoaded', function (){
-    let advantagesSlider = new Swiper(".top_advantage-slider", {
-        pagination: {
-            el: ".top_advantage-slider-pagination",
-        },
-    });
-    let advantagesSliderMobile = new Swiper(".top_advantage-slider-mobile", {
-        pagination: {
-            el: ".top_advantage-slider-mobile-pagination",
-        },
-    });
-    let otherVacanciesSliderMobile = new Swiper(".other-vacancies_slider", {
+document.addEventListener('DOMContentLoaded', function () {
+  let advantagesSlider = new Swiper('.top_advantage-slider', {
+    pagination: {
+      el: '.top_advantage-slider-pagination',
+    },
+  });
+  let advantagesSliderMobile = new Swiper('.top_advantage-slider-mobile', {
+    pagination: {
+      el: '.top_advantage-slider-mobile-pagination',
+    },
+  });
+  let otherVacanciesSliderMobile = new Swiper('.other-vacancies_slider', {
+    slidesPerView: 3.2,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.other-vacancies_nav-btn.next',
+      prevEl: '.other-vacancies_nav-btn.prev',
+    },
+    autoplay: {
+      delay: 2500, // 1.5 секунды
+      disableOnInteraction: false, // продолжать автопрокрутку после взаимодействия
+    },
+    loop: true, // зациклить слайдер (рекомендуется для автопрокрутки)
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      800: {
+        slidesPerView: 2.2,
+      },
+      1024: {
         slidesPerView: 3.2,
-        spaceBetween: 30,
-        navigation: {
-            nextEl: ".other-vacancies_nav-btn.next",
-            prevEl: ".other-vacancies_nav-btn.prev",
-        },
-        breakpoints: {
-            0:{
-                slidesPerView: 1
-            },
-            800:{
-                slidesPerView: 2.2
-            },
-            1024:{
-                slidesPerView: 3.2
-            }
-        }
-    });
-})
+      },
+    },
+  });
+});
